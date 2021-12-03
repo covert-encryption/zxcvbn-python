ce953474380115d62ea6e8370b05960ab59ffe56
foonoxous <RWQe4qDs7LIsmI7Q94ctNMSfwfDW8eld24YGevEzQEKZyfJz1NPlH6uf>
2021-12-03 21:57:52 +0000

    Split words and remove punctuation of user input.

--- zxcvbn/__init__.py
+++ zxcvbn/__init__.py
@@ -1,3 +1,4 @@
+import re
 import time
 from datetime import timedelta
 from decimal import Decimal
@@ -9,8 +10,14 @@ from . import feedback, matching, scoring, time_estimates, types
 def zxcvbn(password: str, user_inputs: Iterable[str] = None) -> types.Feedback:
     start = time.perf_counter()
 
-    # Find unique non-empty user inputs, lower-cased, preserving original order
-    sanitized_inputs = list(dict.fromkeys(s.lower() for s in user_inputs or [] if s))
+    # Find unique non-empty user inputs, split words lower-cased, preserving order
+    wordsep = re.compile(r"\W+|_")
+    sanitized_inputs = list(dict.fromkeys(
+        s.lower()
+        for u in user_inputs or []
+        for s in re.split(wordsep, u)
+        if s
+    ))
     ranked_dictionaries = matching.RANKED_DICTIONARIES
     ranked_dictionaries["user_inputs"] = matching.build_ranked_dict(sanitized_inputs)
 
